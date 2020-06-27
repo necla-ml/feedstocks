2e378919c2cd338ddc38c64ad95016d4a2b99641
Farley Lai <farleylai@nec-labs.com>
2020-06-27 01:07:10 -0400

    Leave on pip temps

--- recipes/ml-ws/build.sh
+++ recipes/ml-ws/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 make kvs3-build kvs3-install kvs3-cffi
-pip install -vvv --no-clean --no-deps --ignore-installed --no-build-isolation --no-binary ':all:' .
+pip install -vvv --no-deps --ignore-installed --no-build-isolation --no-binary ':all:' .
