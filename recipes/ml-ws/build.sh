86c6046470f201153f604f8e4e63cf740ced0b00
Farley Lai <farleylai@nec-labs.com>
2020-07-19 04:53:25 -0400

    Add .condarc

--- recipes/ml-ws/build.sh
+++ recipes/ml-ws/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-make kvs3-build kvs3-install kvs3-cffi
+# make kvs3-build kvs3-install kvs3-cffi
+make kvs3-install kvs3-cffi
 pip install -vvv --no-deps --ignore-installed --no-build-isolation --no-binary ':all:' .
