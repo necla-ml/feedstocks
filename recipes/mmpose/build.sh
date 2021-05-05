9785b642cc6a8d57ad8f2c50f472d17b41b815f9
Farley Lai <farleylai@nec-labs.com>
2021-05-05 04:20:07 -0400

    Fix typo

--- recipes/mmpose/build.sh
+++ recipes/mmpose/build.sh
@@ -11,4 +11,4 @@ XTCOCOTOOLS=https://files.pythonhosted.org/packages/84/93/26a2e38034f5cee8640352
 pip install $CHUMPY $XTCOCOTOOLS -vvv --no-deps --ignore-installed --no-build-isolation
 
 MMPOSE=https://files.pythonhosted.org/packages/c1/8e/71b5a8214cf1656ab92e0de31b8d0246192a4a7d99c6dbf9264365f15dd9/mmpose-$PKG_VERSION-py2.py3-none-any.whl
-pip installed $MMPOSE  -vvv --no-deps --ignore-installed --no-build-isolation 
+pip install $MMPOSE  -vvv --no-deps --ignore-installed --no-build-isolation 
